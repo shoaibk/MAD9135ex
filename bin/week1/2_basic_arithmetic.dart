f724b1138a2b8f26a62693992aa0daf47dbd15a2
zhan0858 <zhan0858@algonquinlive.com>
2024-09-09 20:58:19 -0400

    2-5 exercise

--- bin/week1/2_basic_arithmetic.dart
+++ bin/week1/2_basic_arithmetic.dart
@@ -8,4 +8,11 @@
 // Multiplication: 50
 // Division: ...
 
-void main() {}
+void main() {
+	int num1 = 10;
+	int num2 = 5;
+	print("Addition: ${num1 + num2}");
+	print("Subtraction: ${num1 - num2}");
+	print("Multiplication: ${num1 * num2}");
+	print("Division: ${num1 / num2}");
+}
