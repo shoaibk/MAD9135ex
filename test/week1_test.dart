7dec562cf699012cd06fc7cbb13bacfe984b7304
ade0003 <144292181+ade0003@users.noreply.github.com>
2024-09-09 20:52:54 -0400

    simple change

--- test/week1_test.dart
+++ test/week1_test.dart
@@ -1,6 +1,8 @@
 import 'package:week1/week1.dart';
 import 'package:test/test.dart';
 
+// nothing changed here since the test is not in the test directory yet (it is in the test directory already) so we just create the test directory and
+
 void main() {
   test('calculate', () {
     expect(calculate(), 42);
