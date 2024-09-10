3f198fe6d9c3694e88a305a2a278db62f71f107d
Gina Cabildo <cabi0011@algonquinlive.com>
2024-09-09 20:58:43 -0400

    First round

--- bin/week1/4_loops.dart
+++ bin/week1/4_loops.dart
@@ -1,3 +1,5 @@
 // Objective: Practice using for and while loops.
 
 // Task: Write a program that prints the numbers from 1 to 10 using both a for loop and a while loop.
+
+void main() {}
--- bin/week1/5_functions.dart
+++ bin/week1/5_functions.dart
@@ -2,3 +2,9 @@
 
 // Task: Create a function that takes two integers as parameters, adds them, and returns the result. 
 // Call the function and print the result.
+
+void main () {
+    int a = 2;
+    int b = 4;
+    print(a + b);
+}
